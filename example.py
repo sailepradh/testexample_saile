807c38ba47b36455d82d1586d26570c58762674f
sailepradh <sailepradh@gmail.com>
2020-05-20 10:43:18 +0200

    files added

--- example.py
+++ example.py
@@ -2,17 +2,13 @@ def add(a, b):
     return a + b
 
 def subtract (a,b):
-    return a - b
+    return a + b
 
 def test_add():
     assert add(2, 3) == 5
     assert add('space', 'ship') == 'spaceship'
 
 
-def test_subtract():
-    assert subtract(2, 3) == 1  # <--- fix this in step 8
-
-
 # uncomment the following test in step 5
-#def test_subtract():
-#    assert subtract(2, 3) == -1
+def test_subtract():
+    assert subtract(2, 3) == -1
